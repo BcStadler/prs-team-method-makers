5950e5e5231f37548e0a95f857d9cfca1db9351a
BENJAMIN STADLER <benstadler@gmail.com>
2026-08-20 16:07:06 -0400

    Update Vendors model and controller

--- Prs.Api/Controllers/VendorsController.cs
+++ Prs.Api/Controllers/VendorsController.cs
@@ -24,10 +24,8 @@ namespace Prs.Api.Controllers {
         // GET: api/Vendors/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Vendor>> GetById(int id) {
-            var vendor = await _db.Vendors
-                .Include(vendor => vendor.Products)
-                .FirstOrDefaultAsync(vendor => vendor.Id == id);
-
+            var vendor = await _db.Vendors.FindAsync(id);
+                
             if (vendor == null) {
                 return NotFound();
             }
